fc7e7c00fa78965f62b90309f8219f99d049a1f3
hamid-murambiwa <machipisajunior@gmail.com>
2021-12-01 11:07:30 +0200

    Update index.css import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../styling/index.css';
+import './styling/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
