c73ee073ba1e46dcfdd78c9e0ad8a807b355b587
hamid-murambiwa <machipisajunior@gmail.com>
2021-12-01 11:26:12 +0200

    Update calcHooks.js file

--- src/components/calcHooks.js
+++ src/components/calcHooks.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import calculate from '../logic/calculate';
+import calculate from '../Logic/calculate';
 
 const CalcHooks = () => {
   const [obj, setobj] = useState({ total: 0, next: null, operation: null });
