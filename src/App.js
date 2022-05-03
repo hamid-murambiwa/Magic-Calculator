c547b25f254ce5bde7fb20d73943878d267a42cc
hamid-murambiwa <machipisajunior@gmail.com>
2022-05-03 15:49:51 +0200

    Add the theme change button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
-import { ThemeProvider, useTheme } from './components/features/theme';
+import { ThemeProvider, themes } from './components/features/theme';
 import Header from './components/header';
 import Footer from './components/footer';
 import Calculator from './pages/Calculator';
@@ -11,18 +11,48 @@ import Home from './pages/Home';
 import Quote from './pages/Quote';
 import './styling/app.css';
 
+const STATUS_STATES = ['LIGHT', 'MEDIUM', 'DARK'];
+
 function App() {
-  const [themeName, setThemeName] = useState('main');
-  const theme = useTheme();
+  let themeName = 'light';
+  const theme = themes;
+  const [status, setStatus] = useState(0);
+  const [position, setPosition] = useState(1);
+
+  const handleClick = () => {
+    const newStatus = (status + 1) % STATUS_STATES.length;
+    setStatus(newStatus);
+    if (position < 3) {
+      if (position === 0) {
+        document.getElementById('App').style.backgroundColor = 'rgb(96, 108, 118)';
+      } else if (position === 1) {
+        document.getElementById('App').style.backgroundColor = '#282836';
+      } else if (position === 2) {
+        document.getElementById('App').style.backgroundColor = 'rgb(17, 17, 17)';
+      }
+      const newposition = (position + 1);
+      setPosition(newposition);
+    }
+    if (position === 2) {
+      const newposition = 0;
+      setPosition(newposition);
+    }
+  };
+
+  const text = STATUS_STATES[status];
 
   useEffect(() => {
     // Your theme name determining logic
-    setThemeName('light');
+    themeName = 'dark';
   }, [themeName]);
   return (
     <ThemeProvider value={themeName}>
-      <div className="App" style={{ backgroundColor: theme.background }}>
+      <div id="App" className="App" style={{ backgroundColor: theme[themeName].background }}>
         <Header />
+        <section className="theme">
+          <h4>CLICK TO CHANGE THEME &#8680; </h4>
+          <div aria-hidden="true" onClick={handleClick} id="theme-btn"><p>{text}</p></div>
+        </section>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/quote" element={<Quote />} />
